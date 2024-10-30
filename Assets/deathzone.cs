e1967972de4b9923cb5e1263c4cd16aacbdc0c69
Alithecook <anageotte@ltu.edu>
2024-10-30 02:47:32 -0400

    I had a tough night

---
+++ Assets/deathzone.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathzone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other) {
+        Destroy(other.gameObject);
+    }
+}
